e0284552ccc127484472e9dd561be0a73e623fdc
jinx71 <karim.sazed@gmail.com>
2022-10-20 01:01:46 +0600

    Server setup

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 const data = require('./data/categories.json')
+app.use(cors())
+
 app.get("/categories", (req, res) => {
   res.send(data)
 });
