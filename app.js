2fef83aef62f9bd2af9e63a36cad9c241d3b9311
jinx71 <karim.sazed@gmail.com>
2022-10-20 08:23:36 +0600

    News API, Category API setup

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 const data = require('./data/categories.json')
+const news = require('./data/news.json')
 app.use(cors())
 
 app.get("/categories", (req, res) => {
   res.send(data)
 });
+app.get("/news/:id", (req, res) => {
+  // console.log(req.params.id)
+  const id = req.params.id
+  const selectedNews = news.filter(n => n._id === id)
+  res.send(selectedNews)
+})
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id
 
+  if (id === "08") {
+    res.send(news);
+  }
+  else {
+    const sortedByCategory = news.filter(n => n.category_id === id)
+    res.send(sortedByCategory)
+  }
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
